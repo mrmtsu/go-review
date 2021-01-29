9badc7f68e41ab9c024bdb1adf41661ffa7f81f6
mrmtsu <81202muramatsu@gmail.com>
2021-01-29 20:06:52 +0900

    Array

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var x int = 1
-	xx := float64(x)
-	fmt.Printf("%T %v %f\n", xx, xx, xx)
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a)
 
-	var y float64 = 1.2
-	yy := int(y)
-	fmt.Printf("%T %v %d\n", yy, yy, yy)
+	/*
+		var b [2]int = [2]int{100, 200}
+		// 配列はサイズを変更できない
+		b = append(b, 300)
+		fmt.Println(b)
+	*/
 
-	var s string = "14"
-	i, _ := strconv.Atoi(s)
-	fmt.Printf("%T %v\n", i, i)
-
-	h := "Hello World"
-	fmt.Println(string(h[0]))
+	// スライスは変更できる
+	var b []int = []int{100, 200}
+	b = append(b, 300)
+	fmt.Println(b)
 }
