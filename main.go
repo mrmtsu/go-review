5f9f943d171320ac0496a91f5c3831604703fa16
mrmtsu <81202muramatsu@gmail.com>
2021-01-30 23:05:36 +0900

    slice

--- main.go
+++ main.go
@@ -5,20 +5,24 @@ import (
 )
 
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
 
-	/*
-		var b [2]int = [2]int{100, 200}
-		// 配列はサイズを変更できない
-		b = append(b, 300)
-		fmt.Println(b)
-	*/
+	n[2] = 100
+	fmt.Println(n)
 
-	// スライスは変更できる
-	var b []int = []int{100, 200}
-	b = append(b, 300)
-	fmt.Println(b)
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board)
+
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println(n)
 }
