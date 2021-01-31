34d4f2aaee2627af9269c02d848189a8d4ab04cc
mrmtsu <81202muramatsu@gmail.com>
2021-01-31 23:00:51 +0900

    make

--- main.go
+++ main.go
@@ -5,24 +5,27 @@ import (
 )
 
 func main() {
-	n := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(n)
-	fmt.Println(n[2])
-	fmt.Println(n[2:4])
-	fmt.Println(n[:2])
-	fmt.Println(n[2:])
-	fmt.Println(n[:])
+	n := make([]int, 3, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	n = append(n, 0, 0)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	n = append(n, 1, 2, 3, 4, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	n[2] = 100
-	fmt.Println(n)
+	a := make([]int, 3)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
 
-	var board = [][]int{
-		[]int{0, 1, 2},
-		[]int{3, 4, 5},
-		[]int{6, 7, 8},
+	b := make([]int, 0) // 0のスライスをメモリに確保する
+	var c []int         // nil　メモリに確保しない
+
+	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	// c = make([]int, 5)
+	c = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
 	}
-	fmt.Println(board)
-
-	n = append(n, 100, 200, 300, 400)
-	fmt.Println(n)
+	fmt.Println(c)
 }
