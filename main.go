3530c47f5851863e3566e46df1bd2b989ca58de1
mrmtsu <81202muramatsu@gmail.com>
2021-02-01 23:09:40 +0900

    make

--- main.go
+++ main.go
@@ -5,27 +5,34 @@ import (
 )
 
 func main() {
-	n := make([]int, 3, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 0, 0)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 1, 2, 3, 4, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["new"] = 500
+	fmt.Println(m)
 
-	a := make([]int, 3)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	fmt.Println(m["nothing"])
 
-	b := make([]int, 0) // 0のスライスをメモリに確保する
-	var c []int         // nil　メモリに確保しない
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
 
-	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
 
-	// c = make([]int, 5)
-	c = make([]int, 0, 5)
-	for i := 0; i < 5; i++ {
-		c = append(c, i)
-		fmt.Println(c)
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
+
+	/*
+		var m3 map[string]int
+		m3["pc"] = 5000
+		fmt.Println(m3)
+	*/
+
+	var s []int
+	if s == nil {
+		fmt.Println("Nil")
 	}
-	fmt.Println(c)
 }
