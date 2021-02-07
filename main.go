4e0291da81e54e1e4d19f095cec2d62ae2971cc8
mrmtsu <81202muramatsu@gmail.com>
2021-02-07 20:55:01 +0900

    for

--- main.go
+++ main.go
@@ -2,42 +2,28 @@ package main
 
 import "fmt"
 
-func by2(num int) string {
-	if num%2 == 0 {
-		return "ok"
-	} else {
-		return "no"
-	}
-}
-
 func main() {
-	result := by2(10)
-
-	if result == "ok" {
-		fmt.Println("great")
-	}
-	fmt.Println(result)
-
-	if result2 := by2(10); result2 == "ok" {
-		fmt.Println("greate 2")
-	}
-	/*
-		num := 6
-		if num%2 == 0 {
-			fmt.Println("by 2")
-		} else if num%3 == 0 {
-			fmt.Println("by 3")
-		} else {
-			fmt.Println("else")
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			fmt.Println("continue")
+			continue
 		}
-	*/
 
-	x, y := 10, 10
-	if x == 10 && y == 10 {
-		fmt.Println("&&")
+		if i > 5 {
+			fmt.Println("break")
+			break
+		}
+		fmt.Println(i)
 	}
 
-	if x == 10 || y == 10 {
-		fmt.Println("||")
+	sum := 1
+	for sum < 10 {
+		sum += sum
+		fmt.Println(sum)
+	}
+	fmt.Println(sum)
+
+	for {
+		fmt.Println("hello")
 	}
 }
