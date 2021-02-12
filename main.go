49a17bf447f704c3bc48946fecf5b5cbf1b0c1f6
mrmtsu <81202muramatsu@gmail.com>
2021-02-12 14:53:27 +0900

    error

--- main.go
+++ main.go
@@ -2,29 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-func LoggingSettings(logFile string) {
-	logfile, _ := os.Open(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutput(multiLogFile)
-}
-
 func main() {
-	LoggingSettings("test.log")
-	_, err := os.Open("fdafdsafa")
+	file, err := os.Open("./lesson.go")
 	if err != nil {
-		log.Fatalln("Exit", err)
+		log.Fatal("Error!")
 	}
-	log.Println("logging!")
-	log.Printf("%T %v", "test", "test")
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal("Error!")
+	}
+	fmt.Println(count, string(data))
 
-	log.Fatalf("%T %v", "test", "test")
-	log.Fatalln("error!")
-
-	fmt.Println("ok!")
+	err = os.Chdir("test")
+	if err != nil {
+		log.Fatal("Error")
+	}
 }
